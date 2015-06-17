770a8d15f52037ed08f194d9569062f6b3b1a778
Andrew VanVlack <A.VanVlack@Gmail.com>
2015-06-17 02:26:58 -0700

    Change to git

--- js/app.js
+++ js/app.js
@@ -1,4 +1,40 @@
-$(".menu").css("display", "none");
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $("header .menubar").css("top", -50);
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $("header .menubar").css("top", 0);
+        }
+    }
+
+    lastScrollTop = st;
+}
 
 
 $(function () {
@@ -7,12 +43,4 @@ $(function () {
         $(this).toggleClass('x');
         $("nav").fadeToggle(300);
     });
-});
-
-$(document).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $("header .menubar").css("top", 0);
-    } else {
-        $("header .menubar").css("top", -50);
-    }
 });
